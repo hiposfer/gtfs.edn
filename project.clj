34066c3c39de4f3a11dcf9625488972268de865c
Camilo Roca <camilo.roca@cobi.bike>
2018-09-09 16:35:48 +0200

    helper namespace created README updated clojurescript support added travis ci support added cosmetic changes

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject hiposfer/gtfs.edn "0.1.0"
-  :description "A simple gtfs to parse the Markdown gtfs reference into edn data"
+  :description "A simple gtfs library that exposes the GTFS spec as edn data"
   :url "https://github.com/hiposfer/kamal"
   :license {:name "LGPLv3"
             :url "https://github.com/hiposfer/kamal/blob/master/LICENSE"}
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
+                 [org.clojure/clojurescript "1.9.946" :scope "provided"]]
   :profiles {:dev {:dependencies [[markdown2clj "0.1.3"]]
                    :plugins [[jonase/eastwood "0.2.6"]]}})
